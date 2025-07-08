3fad8ce8eea8ac34b6b5bc2b8259f1927ae8ea97
여철준 <jun091027naver.com>
2025-07-08 16:39:15 +0900

    f

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(session({
     retries: 0
   }),
 }))
+app.use('/images', express.static(path.join(__dirname, 'public')));
 app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
     // process.chdir();
